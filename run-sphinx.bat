5d20c527229184fd8b96abf0c6ddb0033409c460
Stegeman <lstegema@ndc.nasa.gov>
2023-10-02 16:16:32 -0500

    tested both reporting only and full SPHINX runs

--- run-sphinx.bat
+++ run-sphinx.bat
@@ -1,7 +1,6 @@
 @ECHO OFF
 
 SET PYTHONPATH=%cd%
-Rem C:\Users\lstegema\AppData\Local\anaconda3\python.exe .\bin\sphinx.py --ModelList ..\lists\SEPVAL_ReportDevelopment.txt --DataList ..\lists\obs_list_GOES_SC24_25.txt > ..\output\log_SEPVAL_ReportDevelopment.txt
+C:\Users\lstegema\AppData\Local\anaconda3\python.exe .\bin\sphinx.py --ModelList ..\lists\SEPVAL_ReportDevelopment.txt --DataList ..\lists\obs_list_GOES_SC24_25.txt > ..\output\log_SEPVAL_ReportDevelopment.txt
 
-C:\Users\lstegema\AppData\Local\anaconda3\python.exe .\bin\sphinx.py --ModelList ..\lists\SEPVAL_ReportDevelopment.txt --DataList ..\lists\obs_list_GOES_SC24_25.txt > ..\output\log_SEPVAL_ReportDevelopment.txt --ReportOnly
  
