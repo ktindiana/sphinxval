59bf43d17a7fe34948a7bb4735269048afe96dec
Stegeman <lstegema@ndc.nasa.gov>
2023-10-02 17:05:50 -0500

    modularized reporting method; bin/report.py created

--- run-sphinx-report-only.bat
+++ run-sphinx-report-only.bat
@@ -2,6 +2,4 @@
 
 SET PYTHONPATH=%cd%
 Rem C:\Users\lstegema\AppData\Local\anaconda3\python.exe .\bin\sphinx.py --ModelList ..\lists\SEPVAL_ReportDevelopment.txt --DataList ..\lists\obs_list_GOES_SC24_25.txt > ..\output\log_SEPVAL_ReportDevelopment.txt
-
-C:\Users\lstegema\AppData\Local\anaconda3\python.exe .\bin\sphinx.py --ModelList ..\lists\SEPVAL_ReportDevelopment.txt --DataList ..\lists\obs_list_GOES_SC24_25.txt > ..\output\log_SEPVAL_ReportDevelopment.txt --ReportOnly
- 
+C:\Users\lstegema\AppData\Local\anaconda3\python.exe .\bin\report.py
