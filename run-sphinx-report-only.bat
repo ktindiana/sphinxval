51efccbc4365a06e3bf966fffb296596263869ae
Stegeman <lstegema@ndc.nasa.gov>
2023-10-11 12:29:12 -0500

    removed batch files

--- run-sphinx-report-only.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-
-SET PYTHONPATH=%cd%
-Rem C:\Users\lstegema\AppData\Local\anaconda3\python.exe .\bin\sphinx.py --ModelList ..\lists\SEPVAL_ReportDevelopment.txt --DataList ..\lists\obs_list_GOES_SC24_25.txt > ..\output\log_SEPVAL_ReportDevelopment.txt
-C:\Users\lstegema\AppData\Local\anaconda3\python.exe .\bin\report.py
