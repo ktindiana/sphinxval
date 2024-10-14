e3c7fea1be5618cd2e666b6977e2a60663b14363
Ricky Egeland <ricky.egeland@gmail.com>
2024-10-14 15:29:22 -0500

    Fix crash when removing resume duplicates (#148)
    When both input duplicates and resume duplicates are present,
    sphinx would crash because the first duplicate removal changed
    the index order of the Forecast object array, which was saved
    and reused in a dataframe for the resume duplicate removal.
    
    Changed to regenerate the Forecast object dataframe in
    remove_resume_duplicates() and remove existence of fcast_df
    passed from remove_forecast_duplicates().

--- sphinxval/sphinx/sphinx.py
+++ sphinxval/sphinx/sphinx.py
@@ -77,7 +77,7 @@ def validate(data_list, model_list, top=None, Resume=None):
 
     #Check for duplicates in the set of forecasts and remove any
     logger.info("Checking for and removing duplicate forecasts from the input Model List.")
-    model_objs, fcast_df = duplicates.remove_forecast_duplicates(all_energy_channels, model_objs)
+    model_objs = duplicates.remove_forecast_duplicates(all_energy_channels, model_objs)
 
     #Dictionary containing the location of all the .txt files
     #in the subdirectories below top.
@@ -101,7 +101,7 @@ def validate(data_list, model_list, top=None, Resume=None):
             + Resume)
         r_df = resume.read_in_df(Resume)
         
-        model_objs = duplicates.remove_resume_duplicates(r_df, fcast_df, model_objs)
+        model_objs = duplicates.remove_resume_duplicates(r_df, model_objs)
     ################
     
     
