87e206a6651f05259f6c877989594f1f318aa626
Clayton <clayton.f.allison@nasa.gov>
2026-04-13 15:44:56 -0500

    Fixed Validation Unit tests
    Fixed validation unit tests following associations updates

--- tests/test_validation_resume.py
+++ tests/test_validation_resume.py
@@ -53,110 +53,157 @@ def mock_df_populate(dict):
     """
     
     dict["Model"].append("Test_model_resume_old")
+    dict["Observatory"].append("GOES-16,GOES-16")
     dict["Energy Channel Key"].append("min.10.0.max.-1.0.units.MeV")
     dict["Threshold Key"].append("threshold.1.0.units.1 / (cm2 s sr)")
     dict["Mismatch Allowed"].append(False)
-    dict['Evaluation Status'].append('Forecast is evaluated')
     dict["Prediction Energy Channel Key"].append("min.10.0.max.-1.0.units.MeV")
     dict["Prediction Threshold Key"].append("threshold.1.0.units.1 / (cm2 s sr)")
+    dict['Evaluation Status'].append('Forecast is evaluated')
+    dict["Forecast Issue Time"].append(datetime(year = 2022, month = 4, day = 2, hour =  17, minute = 48, second =4))
     dict["Forecast Source"].append("/home/m_sphinx/data/forecasts/enlil2.9e/2022/SEPMOD.20220402_000000.20220402_174512.20220402_172002/json/SEPMOD.2022-04-02T000000Z.2022-04-02T174804Z.json")
     dict["Forecast Path"].append("/home/m_sphinx/data/forecasts/enlil2.9e/2022/SEPMOD.20220402_000000.20220402_174512.20220402_172002/json/")
-    dict["Forecast Issue Time"].append(datetime(year = 2022, month = 4, day = 2, hour =  17, minute = 48, second =4))
+    
     dict["Prediction Window Start"].append(datetime(year = 2022, month = 4, day = 2, hour= 0, minute = 0))
     dict["Prediction Window End"].append(datetime(year =2022, month=4, day=9, hour= 0, minute=1))
+    dict['Observed SEP Event'].append(datetime(year = 2022, month = 4, day = 2,  hour = 14, minute = 40))
             
-            #OBSERVATIONS
-    dict["Number of CMEs"].append("1")
-    dict["CME Start Time"].append(datetime(year = 2022, month = 4, day = 2, hour = 13, minute =38)) #Timestamp of 1st coronagraph image CME is visible in
-    dict["CME Liftoff Time"].append(""), #Timestamp of coronagraph image with 1st indication of CME liftoff (used by CACTUS)
-    dict["CME Latitude"].append(-15.0)
-    dict["CME Longitude"].append(54.0)
-    dict["CME Speed"].append(1370.0)
-    dict["CME Half Width"].append(45.0)
-    dict["CME PA"].append("")
-    dict["CME Catalog"].append("2022-04-02T13:38:00-CME-001")
-    dict["Number of Flares"].append("0")
-    dict["Flare Latitude"].append("")
-    dict["Flare Longitude"].append("")
-    dict["Flare Start Time"].append("")
-    dict["Flare Peak Time"].append("")
-    dict["Flare End Time"].append("")
-    dict["Flare Last Data Time"].append("")
-    dict["Flare Intensity"].append("")
-    dict["Flare Integrated Intensity"].append("")
-    dict["Flare NOAA AR"].append("")
-    dict["Observatory"].append("GOES-16,GOES-16")
-    dict["Observed Time Profile"].append("./tests/files/observations/validation/resume/GOES-16_integral.2022-04-02T000000Z.10MeV.txt,./tests/files/observations/validation/resume/GOES-16_integral.2022-04-05T034500Z.10MeV.txt") #string of comma
-    #separated filenames ".\tests\files\observations\validation\resume\GOES-16_integral.2022-04-05T034500Z.10MeV.txt"
-    dict["Observed SEP All Clear"].append("False")
-    dict["Observed SEP Probability"].append(1.0)
-    dict["Observed SEP Threshold Crossing Time"].append(datetime(year = 2022, month = 4, day = 2,  hour = 14, minute = 40))
-    dict["Observed SEP Start Time"].append(datetime(year = 2022, month = 4, day = 2,  hour = 14, minute = 40))
-    dict["Observed SEP End Time"].append(datetime(year = 2022, month = 4, day = 3,  hour = 0, minute = 10))
-    dict["Observed SEP Duration"].append(9.5)
-    dict["Observed SEP Fluence"].append(7608871.653746902)
-    dict["Observed SEP Fluence Units"].append("1 / cm2")
-    dict["Observed SEP Fluence Spectrum"].append("[{'energy_min': 1, 'energy_max': -1, 'fluence': 82103217.4768976}, {'energy_min': 5, 'energy_max': -1, 'fluence': 17401577.903146088}, {'energy_min': 10, 'energy_max': -1, 'fluence': 7608871.653746902}, {'energy_min': 30, 'energy_max': -1, 'fluence': 1637019.4533599885}, {'energy_min': 50, 'energy_max': -1, 'fluence': 644477.238211527}, {'energy_min': 100, 'energy_max': -1, 'fluence': 216056.99133072246}]")
-    dict["Observed SEP Fluence Spectrum Units"].append("1 / cm2")
-    dict["Observed SEP Peak Intensity (Onset Peak)"].append(32.18270492553711)
-    dict["Observed SEP Peak Intensity (Onset Peak) Units"].append("1 / cm2")
-    dict["Observed SEP Peak Intensity (Onset Peak) Time"].append(datetime(year = 2022, month = 4, day = 2, hour = 16, minute = 0))
-    dict["Observed SEP Peak Intensity Max (Max Flux)"].append(32.18270492553711)
-    dict["Observed SEP Peak Intensity Max (Max Flux) Units"].append("1 / cm2")
-    dict["Observed SEP Peak Intensity Max (Max Flux) Time"].append(datetime(year = 2022, month = 4, day = 2, hour = 16, minute = 0))
 
-    dict["Observed Point Intensity"].append(None)
-    dict["Observed Point Intensity Units"].append(None)
-    dict["Observed Point Intensity Time"].append(None)
-    dict["Observed Max Flux in Prediction Window"].append(32.18270492553711)
-    dict["Observed Max Flux in Prediction Window Units"].append("1 / cm2")
-    dict["Observed Max Flux in Prediction Window Time"].append(datetime(year = 2022, month = 4, day = 2, hour = 16, minute = 0))
-            
-            #PREDICTIONS
-    dict["Predicted SEP All Clear"].append("False")
+    dict["Last Eruption Time"].append("2022-04-02 13:38:00") #Last time for flare/CME
+    dict["Last Trigger Time"].append("2022-04-02 13:38:00")
+    dict["Last Input Time"].append("None")
+    
+    dict["Last Data Time to Issue Time"].append(250.06666666666666)
+            #OBSERVATIONS
+    dict["Prediction Number of CMEs"].append("1")
+    dict["Prediction CME Start Time"].append(datetime(year = 2022, month = 4, day = 2, hour = 13, minute =38)) #Timestamp of 1st coronagraph image CME is visible in
+    dict["Prediction CME Liftoff Time"].append(""), #Timestamp of coronagraph image with 1st indication of CME liftoff (used by CACTUS)
+    dict["Prediction CME Latitude"].append(-15.0)
+    dict["Prediction CME Longitude"].append(54.0)
+    dict["Prediction CME Speed"].append(1370.0)
+    dict["Prediction CME Half Width"].append(45.0)
+    dict["Prediction CME PA"].append("")
+    dict["Prediction CME Catalog"].append("2022-04-02T13:38:00-CME-001")
+    dict['Prediction CME Catalog ID'].append("")
+    dict["Observed SEP CME Start Time"].append(datetime(year = 2022, month = 4, day = 2, hour = 13, minute =38)) #Timestamp of 1st coronagraph image CME is visible in
+    dict["Observed SEP CME Liftoff Time"].append(""), #Timestamp of coronagraph image with 1st indication of CME liftoff (used by CACTUS)
+    dict["Observed SEP CME Latitude"].append(-15.0)
+    dict["Observed SEP CME Longitude"].append(54.0)
+    dict["Observed SEP CME Speed"].append(1370.0)
+    dict["Observed SEP CME Half Width"].append(45.0)
+    dict["Observed SEP CME PA"].append("")
+    dict["Observed SEP CME Catalog"].append("2022-04-02T13:38:00-CME-001")
+    dict['Observed SEP CME Catalog ID'].append("")
+    dict["Prediction Number of Flares"].append("0")
+    dict["Prediction Flare Latitude"].append("")
+    dict["Prediction Flare Longitude"].append("")
+    dict["Prediction Flare Start Time"].append("")
+    dict["Prediction Flare Peak Time"].append("")
+    dict["Prediction Flare End Time"].append("")
+    dict["Prediction Flare Last Data Time"].append("")
+    dict["Prediction Flare Intensity"].append("")
+    dict["Prediction Flare Integrated Intensity"].append("")
+    dict["Prediction Flare NOAA AR"].append("")
+    dict["Observed SEP Flare Latitude"].append("")
+    dict["Observed SEP Flare Longitude"].append("")
+    dict["Observed SEP Flare Start Time"].append("")
+    dict["Observed SEP Flare Peak Time"].append("")
+    dict["Observed SEP Flare End Time"].append("")
+    dict["Observed SEP Flare Intensity"].append("")
+    dict["Observed SEP Flare Integrated Intensity"].append("")
+    dict["Observed SEP Flare NOAA AR"].append("")
+    #separated filenames ".\tests\files\observations\validation\resume\GOES-16_integral.2022-04-05T034500Z.10MeV.txt"
+          
     dict["All Clear Match Status"].append("SEP Event")
+    dict["Predicted SEP All Clear"].append("False")
+    dict["Observed SEP All Clear"].append("False")
+    dict["Predicted SEP All Clear Probability Threshold"].append(None)
+
+
     dict["Predicted SEP Probability"].append(None)
     dict["Probability Match Status"].append("SEP Event")
-    dict["Predicted SEP All Clear Probability Threshold"].append(None)
+    dict["Observed SEP Probability"].append(1.0)
+
     dict["Predicted SEP Threshold Crossing Time"].append(np.datetime64('NaT'))
     dict["Threshold Crossing Time Match Status"].append("SEP Event")
+    dict["Observed SEP Threshold Crossing Time"].append(datetime(year = 2022, month = 4, day = 2,  hour = 14, minute = 40))
+
     dict["Predicted SEP Start Time"].append(np.datetime64("NaT") )
     dict["Start Time Match Status"].append("SEP Event")
+    dict["Observed SEP Start Time"].append(datetime(year = 2022, month = 4, day = 2,  hour = 14, minute = 40))
+
+
     dict["Predicted SEP End Time"].append(np.datetime64('NaT'))
     dict["End Time Match Status"].append("SEP Event")
+    dict["Observed SEP End Time"].append(datetime(year = 2022, month = 4, day = 3,  hour = 0, minute = 10))
+
     dict["Predicted SEP Duration"].append(48)
     dict["Duration Match Status"].append("SEP Event")
+    dict["Observed SEP Duration"].append(9.5)
+
     dict["Predicted SEP Fluence"].append(123)
     dict["Predicted SEP Fluence Units"].append("1 / cm2")
+    dict["Observed SEP Fluence"].append(7608871.653746902)
+    dict["Observed SEP Fluence Units"].append("1 / cm2")
     dict["Fluence Match Status"].append("SEP Event")
+
+
     dict["Predicted SEP Fluence Spectrum"].append(None)
     dict["Predicted SEP Fluence Spectrum Units"].append("")
     dict["Fluence Spectrum Match Status"].append("SEP Event")
+    dict["Observed SEP Fluence Spectrum"].append("[{'energy_min': 1, 'energy_max': -1, 'fluence': 82103217.4768976}, {'energy_min': 5, 'energy_max': -1, 'fluence': 17401577.903146088}, {'energy_min': 10, 'energy_max': -1, 'fluence': 7608871.653746902}, {'energy_min': 30, 'energy_max': -1, 'fluence': 1637019.4533599885}, {'energy_min': 50, 'energy_max': -1, 'fluence': 644477.238211527}, {'energy_min': 100, 'energy_max': -1, 'fluence': 216056.99133072246}]")
+    dict["Observed SEP Fluence Spectrum Units"].append("1 / cm2")
+
+
     dict["Predicted SEP Peak Intensity (Onset Peak)"].append(58.82)
     dict["Predicted SEP Peak Intensity (Onset Peak) Units"].append("1 / cm2")
     dict["Predicted SEP Peak Intensity (Onset Peak) Time"].append(datetime(year = 2022, month = 4, day = 2, hour = 20, minute =30))
+    dict["Observed SEP Peak Intensity (Onset Peak)"].append(32.18270492553711)
+    dict["Observed SEP Peak Intensity (Onset Peak) Units"].append("1 / cm2")
+    dict["Observed SEP Peak Intensity (Onset Peak) Time"].append(datetime(year = 2022, month = 4, day = 2, hour = 16, minute = 0))
     dict["Peak Intensity Match Status"].append("SEP Event")
+
     dict["Predicted SEP Peak Intensity Max (Max Flux)"].append(58.82)
     dict["Predicted SEP Peak Intensity Max (Max Flux) Units"].append("1 / cm2")
     dict["Predicted SEP Peak Intensity Max (Max Flux) Time"].append(datetime(year = 2022, month = 4, day = 2, hour = 20, minute =30))
     dict["Peak Intensity Max Match Status"].append("SEP Event")
+    dict["Observed SEP Peak Intensity Max (Max Flux)"].append(32.18270492553711)
+    dict["Observed SEP Peak Intensity Max (Max Flux) Units"].append("1 / cm2")
+    dict["Observed SEP Peak Intensity Max (Max Flux) Time"].append(datetime(year = 2022, month = 4, day = 2, hour = 16, minute = 0))
             
     dict["Predicted Point Intensity"].append(None)
     dict["Predicted Point Intensity Units"].append(None)
     dict["Predicted Point Intensity Time"].append("SEP Event")
+    dict["Observed Point Intensity"].append(None)
+    dict["Observed Point Intensity Units"].append(None)
+    dict["Observed Point Intensity Time"].append(None)
+
+
+    dict["Observed Max Flux in Prediction Window"].append(32.18270492553711)
+    dict["Observed Max Flux in Prediction Window Units"].append("1 / cm2")
+    dict["Observed Max Flux in Prediction Window Time"].append(datetime(year = 2022, month = 4, day = 2, hour = 16, minute = 0))
 
     dict["Predicted Time Profile"].append("./tests/files/forecasts/validation/resume/SEPMOD.2022-04-02T000000Z.2022-04-02T174804Z.10mev.txt")
     dict["Time Profile Match Status"].append("SEP Event")
+    dict["Observed Time Profile"].append("./tests/files/observations/validation/resume/GOES-16_integral.2022-04-02T000000Z.10MeV.txt,./tests/files/observations/validation/resume/GOES-16_integral.2022-04-05T034500Z.10MeV.txt") #string of comma
             
-    dict["Last Data Time to Issue Time"].append(250.06666666666666)
+    
           
             #MATCHING INFORMATION
-    dict["Last Eruption Time"].append("2022-04-02 13:38:00") #Last time for flare/CME
-    dict["Last Trigger Time"].append("2022-04-02 13:38:00")
-    dict["Last Input Time"].append("None")
+    dict["Overlapping Observations"].append(str([]))
+    dict["All Thresholds in Prediction"].append([config_tests.mm_pred_threshold])
     dict["Threshold Crossed in Prediction Window"].append([True, False])
+    dict["All Threshold Crossing Times"].append([np.datetime64('NaT')])
     dict["Eruption before Threshold Crossed"].append([True, None])
     dict["Time Difference between Eruption and Threshold Crossing"].append([-1.0333333333333334, np.nan])
+    dict['Farside'].append("")
+    dict['Is Source Flare'].append("")
+    dict['All Observation Flare Peak Times'].append(str([]))
+    dict['All Prediction Flares'].append("")
+    dict['Is Source CME'].append("")
+    dict["All Observation CME Start Times"].append("")
+    dict['All Prediction CMEs'].append("")
+    dict["Eruption in Range"].append([True])
     dict["Triggers before Threshold Crossing"].append([True, None])
     dict["Inputs before Threshold Crossing"].append([None, None])
     dict["Triggers before Peak Intensity"].append([True, None])
@@ -173,11 +220,18 @@ def mock_df_populate(dict):
     dict["Time Difference between Inputs and SEP End"].append([None, None])
     dict["Prediction Window Overlap with Observed SEP Event"].append([True, False])
     dict["Ongoing SEP Event"].append([False, None])
-    dict["All Thresholds in Prediction"].append([config_tests.mm_pred_threshold])
-    dict["All Threshold Crossing Times"].append([np.datetime64('NaT')])
-    dict["Eruption in Range"].append([True])
     dict["Original Model Short Name"].append("Test_model_resume_old")
     dict['Trigger Advance Time'].append(['NaT'])
+
+
+    
+   
+    
+    
+    
+    
+    
+    
 
     return dict
 
@@ -357,7 +411,8 @@ class Test_Resume(unittest.TestCase):
         self.Resume = './tests/files/fake_resume_dataframe.pkl'
         mock_dict = validate.initialize_sphinx_dict()
         data = mock_df_populate(mock_dict)
-        
+        print(len(data))
+        print(len(mock_dict))
         df = pd.DataFrame(data = data)
             # if 'ValueError' occurs here, make sure the dictionary that is initialized in validation.py has the same
             # length/information as what get put in there form mock_df_populate. New columns to the dataframe will
