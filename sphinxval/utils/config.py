368b2ab6365002684dfeb293327de4cc85a7a6f9
Clayton Allison <68346264+claya71@users.noreply.github.com>
2026-09-14 09:51:08 -0500

    Trigger dums (#222)
    The full DUM workflow. DUMs are pseudo-models that are created after reading in all predictions/observations into SPHINX and matching. There are ~20 different DUM submodels but generally they fall into two different camps: DUM protons and DUM FAR50/HIT50.
    
    DUM proton models are triggered by observed SEP events. For every SEP event (for each energy channel that crossed its respective threshold), a DUM will be generated and inserted into the SPHINX dataframe. These DUMs will be from the Canonical Profile and Median Peak. These DUMs are best for comparing flux metrics (peaks, time profile).
    DUM FAR50/HIT50 are triggered by every unique predicted trigger. For every flare or CME used by the normal models, there will be a DUM prediction. These DUMs were designed to be comparable to the contingency metrics. For every flare (CME), if the flare class (CME speed) exceeds the class (speed) threshold, then the prediction will be All Clear False, otherwise its True. These thresholds were determined by empirical studies of observed flares/CMEs.
    A subset of DUMs have the _long title, which means they use a source longitude to issue a longitudinal-dependent Canonical Profile.
    
    Once a DUM prediction has been added to the SPHINX dataframe, it is treated identically to all other models.

--- sphinxval/utils/config.py
+++ sphinxval/utils/config.py
@@ -163,3 +163,11 @@ git_commit_sha = git_repo.head.object.hexsha
 git_is_dirty = git_repo.is_dirty()
 git_changed_files = [item.a_path for item in git_repo.index.diff(None)]
 git_untracked_files = git_repo.untracked_files
+
+
+
+# DUM Model configs
+dum_toggle =  True
+triggered_dums = True
+proton_dums = True
+dumpath = os.path.join('static_data', 'DUMs')
