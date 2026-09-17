4d87ef5bf6b5a1d84ebe1f9e5c0c130fbaf464f9
Clayton Allison <68346264+claya71@users.noreply.github.com>
2026-09-17 16:21:44 -0500

    DUM Updates for AWS (#224)
    * DUM Updates for AWS
    
    Slight changes to the DUM model workflow to better conform to
    the AWS workflow. No longer outputs profile outputs that are
    later read in, they are stored in a dictionary and the
    profile_ouptut function appends to other profiles and stored
    in the json to be passed to vivid. Other small changes to
    ensure robustness
    
    * Cleaning Up
    
    Cleaning up some filenames to use config's dumpath so AWS
    needs to only change that to control the filenames, even though
    the files are created they are used as identifiers in the
    validation workflow.

--- sphinxval/utils/dums.py
+++ sphinxval/utils/dums.py
@@ -327,7 +327,6 @@ def canonical_prof_dum(df):
         # Loop over energies
         
         sub_df = df[df["Energy Channel Key"] == available_energies]
-        sub_df.to_csv('testdf.csv')
         trigger_columns = ['Observed SEP CME Start Time', 'Observed SEP CME Longitude', 'Observed SEP CME Latitude', 'Observed SEP CME Liftoff Time', 'Observed SEP CME Speed', 'Observed SEP CME Half Width', 'Observed SEP CME PA',\
             'Observed SEP CME Catalog', 'Observed SEP Flare Start Time', 'Observed SEP Flare Peak Time', 'Observed SEP Flare End Time', 'Observed SEP Flare Longitude', 'Observed SEP Flare Latitude', 'Observed SEP Flare NOAA AR']
         observed_events = sub_df['Observed SEP Start Time'].unique()
@@ -528,11 +527,11 @@ def canonical_prof_dum(df):
                     output_df = pd.DataFrame(output_dict)
                     output_df = output_df.set_index('dates')
                     start_time_filename = start_time_str.replace('/', '').replace(':','')
-                    output_filename = './model/DUMs/CanonicalProfile/DUM_CanonicalProfile_' + location_string + '_' + available_energies + '_' + start_time_filename + trigger_str + '.txt'
+                    output_filename = os.path.join(cfg.dumpath,'DUM_CanonicalProfile_' + location_string + '_' + available_energies + '_' + start_time_filename + trigger_str + '.txt')
                     
 
                     dum_dict['Forecast Source'] = output_filename
-                    dum_dict['Forecast Path'] = './model/DUMs/CanonicalProfile/'
+                    dum_dict['Forecast Path'] = cfg.dumpath
                     dum_dict['Forecast Issue Time'] = pd.NaT
                     dum_dict['Prediction Window Start'] = un_normalized_time[0]
                     dum_dict['Prediction Window End'] = un_normalized_time[-1]
@@ -829,7 +828,7 @@ def triggered_dum_workflow(sphinx_df):
                                     start_time_str = start
                                     start_time_dt = zulu_to_time(start_time_str)
                             elif pd.isnull(start):
-                                foo = [current_event['Prediction Flare Start Time']+ timedelta(hours = 1) if 'flare' in names else current_event['Prediction CME Start Time']+ timedelta(hours = 1)][0]
+                                foo = [robust_timing(current_event['Prediction Flare Start Time'])+ timedelta(hours = 1) if 'flare' in names else robust_timing(current_event['Prediction CME Start Time'])+ timedelta(hours = 1)][0]
                                 start_time_dt = pd.to_datetime(foo)
                                 start_time_str = str(foo)
                                
@@ -868,11 +867,9 @@ def triggered_dum_workflow(sphinx_df):
                             output_df = pd.DataFrame(output_dict)
                             output_df = output_df.set_index('dates')
                             start_time_filename = start_time_str.replace('/', '').replace(':','')
-                            output_filename = './model/DUMs/CanonicalProfile/DUM_CanonicalProfile_' + location_string + '_' + available_energies + '_' + start_time_filename + trigger_str + '.txt'
-                            output_df.to_csv(output_filename, sep='\t', header = False)
-
+                            output_filename = os.path.join(cfg.dumpath, 'DUM_CanonicalProfile_' + location_string + '_' + available_energies + '_' + start_time_filename + trigger_str + '.txt')
                             dum_dict['Forecast Source'] = output_filename
-                            dum_dict['Forecast Path'] = './model/DUMs/CanonicalProfile/'
+                            dum_dict['Forecast Path'] = cfg.dumpath
                             dum_dict['Forecast Issue Time'] = pd.NaT
                             dum_dict['Prediction Window Start'] = un_normalized_time[0]
                             dum_dict['Prediction Window End'] = un_normalized_time[-1]
@@ -942,9 +939,9 @@ def triggered_dum_workflow(sphinx_df):
                             dum_dict['Last Input Time'] = pd.NaT
                             dum_dict['Last Eruption Time'] = last_trig
                             start_time_filename = str(last_trig).replace('/', '').replace(':','')
-                            output_filename = './model/DUMs/CanonicalProfile/DUM_CanonicalProfile_' + available_energies + '_' + start_time_filename + '.txt'
+                            output_filename = os.path.join(cfg.dumpath, 'DUM_CanonicalProfile_' + available_energies + '_' + start_time_filename + '.txt')
                             dum_dict['Forecast Source'] = output_filename
-                            dum_dict['Forecast Path'] = './model/DUMs/CanonicalProfile/'
+                            dum_dict['Forecast Path'] = cfg.dumpath
                             dum_dict['Forecast Issue Time'] = pd.NaT
                             dum_dict['Prediction Window Start'] = last_trig
                             dum_dict['Prediction Window End'] = last_trig + timedelta(hours = 6)
